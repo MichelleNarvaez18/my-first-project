bb0d7fc4915a307c3ded99826f7449de561c365f
MichelleNarvaez18 <lailamichelle18@outlook.es>
2023-11-25 11:23:26 -0500

    feat: add Button with storybook

---
+++ src/home/presentation/atoms/button/Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonProps } from './ButtonProps'
+import './button.css'
+
+const Button = ({ text }: ButtonProps) => {
+  return (
+    <button className='button'>{text}</button>
+  )
+}
+
+export default Button
